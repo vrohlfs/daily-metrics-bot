c37f3609590b534aeac10fa2876b4c75c2a42865
vrohlfs <soubra.vr@gmail.com>
2024-11-04 23:23:09 -0500

    Formatting

--- daily_script.py
+++ daily_script.py
@@ -10,13 +10,13 @@ def calculate_progress():
     start_of_month = datetime(now.year, now.month, 1)
     next_month = now.month % 12 + 1
     start_of_next_month = datetime(now.year + (now.month // 12), next_month, 1)
-    start_of_day = datetime(now.year, now.month, now.day)
-    end_of_day = start_of_day.replace(hour=23, minute=59, second=59)
+    # start_of_day = datetime(now.year, now.month, now.day)
+    # end_of_day = start_of_day.replace(hour=23, minute=59, second=59)
     
     # Calculate percentage progress
     year_progress = (now - start_of_year) / (end_of_year - start_of_year) * 100
     month_progress = (now - start_of_month) / (start_of_next_month - start_of_month) * 100
-    day_progress = (now - start_of_day) / (end_of_day - start_of_day) * 100
+    # day_progress = (now - start_of_day) / (end_of_day - start_of_day) * 100
     life_progress = ((now.year - 1993) / 80) * 100  # Using 1993 as birth year and 80 as lifespan
     return year_progress, month_progress, day_progress, life_progress
 
@@ -27,6 +27,7 @@ def daily_commit():
     # Define the message and house rules
     message = """
     Be good, and be good at it - 1.01^365
+    
     Daily reminder of house rules:
     - Hard Thing Rule
     - Fun Thing Rule
@@ -34,10 +35,10 @@ def daily_commit():
     - No whining
     - No complaining
     - No excuses
+
     Time Metrics:
     - Year: {:.2f}%
     - Month: {:.2f}%
-    - Day: {:.2f}%
     - Life: {:.2f}%
     """.format(year_progress, month_progress, day_progress, life_progress)
     
