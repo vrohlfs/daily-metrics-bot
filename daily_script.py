606ceb0eb40c61b8839f765261060d284f912334
vrohlfs <soubra.vr@gmail.com>
2024-11-04 23:30:34 -0500

    Formatting

--- daily_script.py
+++ daily_script.py
@@ -4,30 +4,28 @@ from datetime import datetime
 
 def calculate_progress():
     now = datetime.now()
-    # Define start and end dates for year, month, and day
+    # Define start and end dates for year, month
     start_of_year = datetime(now.year, 1, 1)
     end_of_year = datetime(now.year + 1, 1, 1)
     start_of_month = datetime(now.year, now.month, 1)
     next_month = now.month % 12 + 1
     start_of_next_month = datetime(now.year + (now.month // 12), next_month, 1)
-    # start_of_day = datetime(now.year, now.month, now.day)
-    # end_of_day = start_of_day.replace(hour=23, minute=59, second=59)
+ 
     
     # Calculate percentage progress
     year_progress = (now - start_of_year) / (end_of_year - start_of_year) * 100
     month_progress = (now - start_of_month) / (start_of_next_month - start_of_month) * 100
-    # day_progress = (now - start_of_day) / (end_of_day - start_of_day) * 100
     life_progress = ((now.year - 1993) / 80) * 100  # Using 1993 as birth year and 80 as lifespan
-    return year_progress, month_progress, day_progress, life_progress
+    return year_progress, month_progress, life_progress
 
 def daily_commit():
     # Calculate progress metrics
-    year_progress, month_progress, day_progress, life_progress = calculate_progress()
+    year_progress, month_progress, life_progress = calculate_progress()
     
     # Define the message and house rules
     message = """
     Be good, and be good at it - 1.01^365
-    
+
     Daily reminder of house rules:
     - Hard Thing Rule
     - Fun Thing Rule
@@ -40,7 +38,7 @@ def daily_commit():
     - Year: {:.2f}%
     - Month: {:.2f}%
     - Life: {:.2f}%
-    """.format(year_progress, month_progress, day_progress, life_progress)
+    """.format(year_progress, month_progress, life_progress)
     
     # Make sure the daily_files directory exists
     os.makedirs('daily_files', exist_ok=True)
